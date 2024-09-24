221790ff0ea3f5d072477a58c9269d99d1c3c8a8
Abdellah <abdallahkaba98@gmail.com>
2024-09-24 00:48:42 +0000

    login for keycloak

---
+++ demo-ui/src/app/pages/login/login.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {KeycloakService} from "../../services/keycloak/keycloak.service";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit{
+
+  constructor(
+    private key: KeycloakService
+  ) {
+  }
+
+  async ngOnInit(): Promise<void> {
+
+    await this.key.init()
+    await this.key.login()
+  }
+
+}
